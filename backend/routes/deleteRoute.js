22ce170ded508af7ba2677183512740d61de8dc1
ThapaVinay <thechampthapa@gmail.com>
2023-12-02 15:50:14 +0530

    deleted unneccessary things from backend

--- backend/routes/deleteRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const deleteController = require('../controllers/deleteController');
-
-router.post('/', deleteController.removeImage); // Changed the route to '/'
-
-module.exports = router;
